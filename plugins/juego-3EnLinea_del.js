2926d80de8b71e0dcf6cf98a020aa2d70bdde02a
Matt Melendez <118824091+Ne-Kosmic@users.noreply.github.com>
2022-11-22 15:09:34 -0500

    ✍️🌌
    E

---
+++ plugins/juego-3EnLinea_del.js
@@ -0,0 +1,18 @@
+let handler = async (m, {
+	conn,
+	usedPrefix,
+	command
+}) => {
+	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
+	if (room == undefined) return
+	delete conn.game[room.id]
+	await m.reply('*Sala 3 en linea eliminado correctamente ✓*')
+}
+
+handler.help = ['delttt']
+handler.tags = ['games']
+handler.command = /^(delttt)$/
+handler.group = true
+handler.limit = true
+
+export default handler
